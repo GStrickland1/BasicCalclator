2121df666e00c17e7efb1967562e29be91afd4b7
GStrickland1 <80374811+GStrickland1@users.noreply.github.com>
2021-04-04 12:18:58 -0700

    Add files via upload

--- Test.c
+++ Test.c
@@ -1,9 +1,10 @@
 // Program to create basic calculator
 #include <stdio.h>
+#include <stdlib.h>
 
 // Union
 union Calc {
-	int add; int num1;
+	int add;
 	int subtract;
 	int multiply;
 	int divide;
@@ -11,41 +12,49 @@ union Calc {
 
 // Main Function
 int main()
-{	
+{
+	FILE * fpr; //File I/O 
+		fpr = fopen_s("data.txt", "r");
+		if (fpr == NULL)
+		{
+			puts("Error while opening file");
+			exit();
+		}
+		fclose(fpr);
+	
 	char Operator;
-	float num1, num2, result = 0;
+	unsigned int num1, num2, result = 0;
 
 	printf("\n Please Enter an Operator (+, -, *, /) : ");
-	scanf_s("%c", &Operator);
+	scanf_s("%c", &Operator, 1);
 
 	printf("\n Please Enter the Values for two Operands: num1 and num2 : ");
-	scanf_s("%f%f", &num1, &num2);
+	scanf_s("%u%u", &num1, &num2);
 
 	if (Operator == '+') /* Addition operation */
 	{
 		/* Display Results */
-		printf("\n The result of %.2f + %.2f = %.2f", num1, num2, num1 + num2);
+		printf("\n The result of %.2u + %.2u = %.2u", num1, num2, num1 + num2);
 	}
 	else if (Operator == '-') /* Subtraction */
 	{
 		/* Display Results */
-		printf("\n The result of %.2f - %.2f = %.2f", num1, num2, num1 - num2);
+		printf("\n The result of %.2u - %.2u = %.2u", num1, num2, num1 - num2);
 	}
 	else if (Operator == '*') /* Multiplication */
 	{
 		/* Display Results */
-		printf("\n The result of %.2f * %.2f = %.2f", num1, num2, num1 * num2);
+		printf("\n The result of %.2u * %.2u = %.2u", num1, num2, num1 * num2);
 	}
 	else if (Operator == '/') /* Division */
 	{
 		/* Display Results */
-		printf("\n The result of %.2f / %.2f = %.2f", num1, num2, num1 / num2);
+		printf("\n The result of %.2u / %.2u = %.2u", num1, num2, num1 / num2);
 	}
 	else
 	{
 		/* Display Results */
 		printf("\n You have entered an Invalid Operator ");
 	}
-
 	return 0;
 }
